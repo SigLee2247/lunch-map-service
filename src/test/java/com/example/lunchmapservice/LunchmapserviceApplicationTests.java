e839387decbf0118318fea493d084978beea6606
Siglee2247 <lgh941107@gmail.com>
2024-04-04 23:05:47 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/example/lunchmapservice/LunchmapserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lunchmapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchmapserviceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
