b05e692db92486a8de8ee01a60a3ae733cdf334e
Siglee2247 <lgh941107@gmail.com>
2024-04-05 01:57:27 +0900

    feat: 이름과 지번 주소로 조회하는 Restaurant 레포지토리 구현 #2

--- src/main/java/com/wanted/lunchmapservice/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/wanted/lunchmapservice/restaurant/repository/RestaurantRepository.java
@@ -1,8 +1,11 @@
 package com.wanted.lunchmapservice.restaurant.repository;
 
 import com.wanted.lunchmapservice.restaurant.entity.Restaurant;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+  Optional<Restaurant> findByNameAndLotNumberAddress(String name, String lotNumberAddress);
 
 }
