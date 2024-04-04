94a15a94c0fd59ba049d4809f25bebbc76d35bcc
Siglee2247 <lgh941107@gmail.com>
2024-04-05 00:42:03 +0900

    feat: Location, Restaurant 도메인 레포지토리 구현 #2

---
+++ src/main/java/com/wanted/lunchmapservice/location/repository/LocationRepository.java
@@ -0,0 +1,9 @@
+package com.wanted.lunchmapservice.location.repository;
+
+
+import com.wanted.lunchmapservice.location.entity.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationRepository extends JpaRepository<Location, Long> {
+
+}
