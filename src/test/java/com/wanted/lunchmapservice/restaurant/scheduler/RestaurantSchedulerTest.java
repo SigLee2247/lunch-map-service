379a5f864863f47f4a6e9c53a81fea01f5010bda
Siglee2247 <lgh941107@gmail.com>
2024-04-05 14:16:09 +0900

    move: 스텁 데이터 위치 변경 #2

--- src/test/java/com/wanted/lunchmapservice/restaurant/scheduler/RestaurantSchedulerTest.java
+++ src/test/java/com/wanted/lunchmapservice/restaurant/scheduler/RestaurantSchedulerTest.java
@@ -24,7 +24,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class RestaurantSchedulerServiceTest {
+class RestaurantSchedulerTest {
 
   @InjectMocks
   RestaurantScheduler restaurantScheduler;
