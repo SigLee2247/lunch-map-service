93d23284bfb254198ae366524af2a408ba1cc285
Siglee2247 <lgh941107@gmail.com>
2024-04-05 22:09:57 +0900

    feat: Restaurant getRestaurant 기능 구현 #1 - controller, service, mapper 구현

--- src/main/java/com/wanted/lunchmapservice/restaurant/service/RestaurantGetService.java
+++ src/main/java/com/wanted/lunchmapservice/restaurant/service/RestaurantGetService.java
@@ -1,16 +1,21 @@
 package com.wanted.lunchmapservice.restaurant.service;
 
 
+import com.wanted.lunchmapservice.common.dto.CustomPage;
 import com.wanted.lunchmapservice.common.dto.ResponseDto;
 import com.wanted.lunchmapservice.common.exception.CommonException;
 import com.wanted.lunchmapservice.restaurant.controller.dto.GetRestaurantDetailResponseDto;
+import com.wanted.lunchmapservice.restaurant.controller.dto.GetRestaurantSimpleResponseDto;
 import com.wanted.lunchmapservice.restaurant.entity.Restaurant;
 import com.wanted.lunchmapservice.restaurant.mapper.RestaurantMapper;
 import com.wanted.lunchmapservice.restaurant.repository.RestaurantRepository;
+import com.wanted.lunchmapservice.restaurant.repository.dto.RestaurantGetFilterDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Service
 @Transactional(readOnly = true)
@@ -20,6 +25,10 @@ public class RestaurantGetService {
   private final RestaurantMapper mapper;
   public ResponseDto<GetRestaurantDetailResponseDto> getDetails(Long restaurantId) {
     return mapper.toResponseDto(validRestaurant(restaurantId));
+  }
+
+  public ResponseDto<CustomPage<GetRestaurantSimpleResponseDto>> getRestaurant(@ModelAttribute RestaurantGetFilterDto request, Pageable pageable) {
+    return mapper.toResponseDto(repository.findPageByFilter(pageable,request));
   }
 
   private Restaurant validRestaurant(Long restaurantId) {
