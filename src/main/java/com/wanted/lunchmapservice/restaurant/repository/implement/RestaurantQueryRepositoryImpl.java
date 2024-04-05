776e8ed0ffa3c3d2e942ef1a2b4a96772519d0cb
Siglee2247 <lgh941107@gmail.com>
2024-04-06 01:28:59 +0900

    feat: nearRestaurant 조회 repository 구현 #6 - Haversine 공식을 활용한 조회 - data양의 증가에 따른 성능 저하 문제 발생 (현재 조회 0.3s)

--- src/main/java/com/wanted/lunchmapservice/restaurant/repository/implement/RestaurantQueryRepositoryImpl.java
+++ src/main/java/com/wanted/lunchmapservice/restaurant/repository/implement/RestaurantQueryRepositoryImpl.java
@@ -4,16 +4,21 @@ import static com.wanted.lunchmapservice.location.entity.QLocation.location;
 import static com.wanted.lunchmapservice.restaurant.entity.QRestaurant.restaurant;
 import static org.springframework.util.ObjectUtils.isEmpty;
 
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberTemplate;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.wanted.lunchmapservice.restaurant.entity.Restaurant;
 import com.wanted.lunchmapservice.restaurant.repository.RestaurantQueryRepository;
 import com.wanted.lunchmapservice.restaurant.repository.dto.RequestRestaurantGetFilterDto;
+import com.wanted.lunchmapservice.restaurant.controller.dto.NearRestaurantRequestDto;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
@@ -21,6 +26,7 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class RestaurantQueryRepositoryImpl implements RestaurantQueryRepository {
   private final JPAQueryFactory query;
+  private static final Integer EARTH_RADIUS = 6371;
 
   @Override
   public Page<Restaurant> findPageByFilter(Pageable pageable, RequestRestaurantGetFilterDto condition){
@@ -44,6 +50,38 @@ public class RestaurantQueryRepositoryImpl implements RestaurantQueryRepository 
         );
 
     return PageableExecutionUtils.getPage(content,pageable, count::fetchOne);
+  }
+
+  @Override
+  public Page<Restaurant> findNearByRestaurant(NearRestaurantRequestDto dto, Pageable pageable) {
+    Double userLat = Double.parseDouble(dto.getCurrentLatitude());
+    Double userLng = Double.parseDouble(dto.getCurrentLongitude());
+
+    List<Restaurant> nearByRestaurants = query
+        .select(restaurant)
+        .from(restaurant)
+        .where(acosExpression(userLat, userLng).loe(dto.getRange()))
+        .orderBy(getOrderByExpression(userLat, userLng, pageable.getSort()))
+        .fetch();
+
+    JPAQuery<Long> count = query.select(restaurant.count())
+        .from(restaurant)
+        .where(acosExpression(userLat, userLng).loe(dto.getRange()));
+
+    return PageableExecutionUtils.getPage(nearByRestaurants, pageable, count::fetchOne);
+  }
+
+
+  //추 후 정렬 조건이 늘어나면 추가 예정
+  private OrderSpecifier<?> getOrderByExpression(Double userLat, Double userLng, Sort sort) {
+    return sort.equals(Sort.unsorted()) ? acosExpression(userLat, userLng).asc() : restaurant.averageScore.desc();
+  }
+
+  private NumberTemplate<Double> acosExpression(Double userLat, Double userLng) {
+    return Expressions.numberTemplate(Double.class,
+        "{0} * acos(cos(radians({1})) * cos(radians({2})) * cos(radians({3}) - radians({4})) + "
+            + "sin(radians({1})) * sin(radians({2})))",
+        EARTH_RADIUS, userLat, restaurant.latitude, userLng, restaurant.longitude);
   }
 
   private BooleanExpression cityNameEq(String cityName) {
